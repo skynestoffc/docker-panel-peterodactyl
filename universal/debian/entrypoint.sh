612ee92e37b2199bf7844e4f0cccf88f114fbcde
siputzx <siputzz.id@gmail.com>
2026-04-03 16:18:50 +0700

    Add Deno and install yt-dlp at startup

--- universal/debian/entrypoint.sh
+++ universal/debian/entrypoint.sh
@@ -9,6 +9,8 @@ export TZ
 INTERNAL_IP=$(ip route get 1 2>/dev/null | awk '{print $(NF-2); exit}')
 export INTERNAL_IP
 
+python3 -m pip install --user --no-cache-dir -U yt-dlp >/tmp/yt-dlp-install.log 2>&1 || true
+
 STARTUP=${STARTUP:-/bin/bash -li}
 
 printf "\033[1m\033[33m%s@tokoptero~ \033[0m%s\n" "$(whoami)" "$STARTUP"
